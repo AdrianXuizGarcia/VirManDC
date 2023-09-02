c3e571056c7450215eae8d4b8cafdf7564514117
AdrianXuiz <adrian.xuizg@udc.es>
2023-09-02 19:05:08 +0200

    Cleanup of libraries, code. Reorganization of the folders of the proyect

--- VirManDC/Assets/VirManDC/Scripts/GameObjectScripts/ControllersList.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ControllersList : MonoBehaviour
-{
-	public IndicatorPanelController_old indicatorPanelcontroller;
-	public WarningController warningController;
-}
